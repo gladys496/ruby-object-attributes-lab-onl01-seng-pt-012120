7f23e48e5d889cea614f8bc4aa0a1bbc1f286300
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-06 20:24:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,10 @@
 require "pry"
 
 class Dog
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
-  def name
-    @this_dogs_name
-  end
-end
-
-fido = Dog.new
-fido.name = "Fido"
-puts fido.name
-
-
-
-
-class Dog
-	attr_accessor  :name, :age, :owner_name
+	attr_accessor  :name, :breed
 		
-		def initialize(name, age, owner_name)
+		def initialize(name, breed)
 			@name = name
-			@age = age
-			@owner_name = owner_name
+			@breed = breed 
+		
 

75500dbe802efd846818c236deb0e6eaf86b434a
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-06 20:15:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,21 +14,5 @@ fido = Dog.new
 fido.name = "Fido"
 puts fido.name
  
-def breed = (dogs_breed)
-  @this_dogs_breed = dogs_breed
-end 
-
-def breed 
-  @this_dogs_breed
-  end
-end 
-
-
-
-husky.breed = "Husky"
-
-
-puts husky.breed 
-
 
 

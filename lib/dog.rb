2df89ef900c709c1fac1218c6e1abff916535e5d
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-06 20:35:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,25 +12,3 @@ class Dog
   puts "#{self.name} says: This dog is a  #{self.breed}."
 	  
 	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  class Dog
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
-  def name
-    @this_dogs_name
-  end
-end
-
-fido = Dog.new
-fido.name = "Fido"
-puts fido.name

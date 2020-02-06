36272ddc5e663a489a3f853ef01ed7fefbcc9a0c
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-06 20:49:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,16 @@
 require "pry"
 
+ class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+puts fido.name

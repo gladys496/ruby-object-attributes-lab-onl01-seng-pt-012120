b2eff7be38dadc873dbe50dce36f6f144b17b2a6
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-06 20:39:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,2 @@
 require "pry"
 
-class Dog
-	attr_accessor  :name, :breed
-		
-		def initialize(name, breed)
-			@name = name
-			@breed = breed 
-		end 
-		
-	def dog_breed
-  puts "#{self.name} says: This dog is a  #{self.breed}."
-	  
-	  

1bf07a6210f04b868083190e5cf4bf488c37349b
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-06 20:23:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,5 +18,11 @@ puts fido.name
 
 
 
-
+class Dog
+	attr_accessor  :name, :age, :owner_name
+		
+		def initialize(name, age, owner_name)
+			@name = name
+			@age = age
+			@owner_name = owner_name
 
